8100e774d8faef2b6d6b486913d70490b091f5d6
mdnazmul42726 <nazmulofficial@outlook.com>
2024-02-04 15:48:15 +0600

    Live chat option has been removed from the application

---
+++ src/components/CallBtn.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import WP from '../assets/x.png';
+
+const CallBtn = () => {
+    const [showNeedHelp, setShowNeedHelp] = useState(false);
+
+    return (
+
+
+        <div className="absolute right-5 bottom-5 !fixed z-[50000]">
+            <div className="flex items-center gap-4">
+
+                {showNeedHelp && <p className='font-semibold text-white'>Need Help?</p>}
+                <a href="https://wa.me/+971503583882" target='_blank' rel='noreferrer' onMouseEnter={() => setShowNeedHelp(true)} onMouseLeave={() => setShowNeedHelp(false)}>  <img className="w-12 cursor-pointer hover:scale-110 transition-all" src={WP} alt="" /></a>
+
+            </div>
+        </div>
+    );
+};
+
+export default CallBtn;
