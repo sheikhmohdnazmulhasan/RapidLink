a450d0de9f5b95175740c6723c4813372900ce04
Habibur1112223 <webdevelopment.habib2023@gmail.com>
2024-02-13 16:50:43 +0600

    Button to rejoin the meeting

--- src/Meeting/components/screens/LeaveScreen.jsx
+++ src/Meeting/components/screens/LeaveScreen.jsx
@@ -6,9 +6,18 @@ const LeaveScreen = () => {
             <div className="bg-gray-800 h-screen flex flex-col flex-1 items-center justify-center">
                   {/* Display a message indicating that the user left the meeting */}
                   <h1 className="text-white text-4xl">You left the meeting!</h1>
+                  {/* Button to rejoin the meeting */}
+                  <div className="mt-12">
+                        <button
+                              className="w-full bg-purple-350 text-white px-16 py-3 rounded-lg text-sm"
+                             
+                              >
+
+                              </button>
 
             </div>
-      );
+            </div>
+                  );
 };
 
-export default LeaveScreen;
+                  export default LeaveScreen;
