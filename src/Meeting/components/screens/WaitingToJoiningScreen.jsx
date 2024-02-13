1391a50e75c72532ab9c949eb7e541fa12869243
Habibur1112223 <webdevelopment.habib2023@gmail.com>
2024-02-13 16:28:34 +0600

    style set up for mobile device

--- src/Meeting/components/screens/WaitingToJoiningScreen.jsx
+++ src/Meeting/components/screens/WaitingToJoiningScreen.jsx
@@ -1,22 +1,37 @@
 import React from 'react';
 
 const WaitingToJoiningScreen = () => {
+
+      // JSX to render the WaitingToJoinScreen component
       return (
             <div
-      className="bg-gray-800"
-      style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        height: "100vh",
-        // backgroundColor: theme.palette.darkTheme.main,
-      }}
-    >
-      
+                  className="bg-gray-800"
+                  style={{
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        height: "100vh",
+                        // backgroundColor: theme.palette.darkTheme.main,
+                  }}
+            >
+                  <div className="flex flex-col">
+                        {/* height width setup for the mobile */}
+                        <div
+                              style={{
+                                    height: isTab ? 200 : isMobile ? 200 : 250,
+                                    width: isTab ? 200 : isMobile ? 200 : 250,
+                              }}
+                        >
+
+                        </div>
+
+
+                  </div>
 
 
 
-    </div>
+
+            </div>
       );
 };
 
