02e33f1c863f7af2a812dd05575ff859c7c8405e
Habibur1112223 <webdevelopment.habib2023@gmail.com>
2024-02-13 16:22:00 +0600

    apply the flex justifycontent set up

---
+++ src/Meeting/components/screens/WaitingToJoiningScreen.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const WaitingToJoiningScreen = () => {
+      return (
+            <div
+      className="bg-gray-800"
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+        // backgroundColor: theme.palette.darkTheme.main,
+      }}
+    >
+      
+
+
+
+    </div>
+      );
+};
+
+export default WaitingToJoiningScreen;
