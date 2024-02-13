8ca138bdb31e778cd6818330a3d4c395310e7a0a
Habibur1112223 <webdevelopment.habib2023@gmail.com>
2024-02-13 16:40:01 +0600

    useEffect to handle message rotation at intervals

--- src/Meeting/components/screens/WaitingToJoiningScreen.jsx
+++ src/Meeting/components/screens/WaitingToJoiningScreen.jsx
@@ -11,53 +11,62 @@ const WaitingToJoiningScreen = () => {
       const [message, setMessage] = useState(waitingMessages[0]);
       // Reference for the interval used to change messages
       const intervalRef = useRef(null);
+      // useEffect to handle message rotation at intervals
+      useEffect(() => {
+            intervalRef.current = setInterval(() => {
+                  setMessage((s) =>
+                        s.index === waitingMessages.length - 1
+                              ? s
+                              : waitingMessages[s.index + 1]
+                  );
+            }, 3000);
 
-      // JSX to render the WaitingToJoinScreen component
-      return (
-            <div
-                  className="bg-gray-800"
-                  style={{
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "center",
-                        height: "100vh",
-                        // backgroundColor: theme.palette.darkTheme.main,
-                  }}
-            >
-                  <div className="flex flex-col">
-                        {/* height width setup for the mobile */}
-                        <div
-                              style={{
-                                    height: isTab ? 200 : isMobile ? 200 : 250,
-                                    width: isTab ? 200 : isMobile ? 200 : 250,
-                              }}
-                        >
-                              {/* Lottie animation component */}
-                              <Lottie
-                                    loop={animationDefaultOptions.loop}
-                                    autoplay={animationDefaultOptions.autoplay}
-                                    animationData={animationDefaultOptions.animationData}
-                                    rendererSettings={{
-                                          preserveAspectRatio:
-                                                animationDefaultOptions.rendererSettings.preserveAspectRatio,
+            // JSX to render the WaitingToJoinScreen component
+            return (
+                  <div
+                        className="bg-gray-800"
+                        style={{
+                              display: "flex",
+                              alignItems: "center",
+                              justifyContent: "center",
+                              height: "100vh",
+                              // backgroundColor: theme.palette.darkTheme.main,
+                        }}
+                  >
+                        <div className="flex flex-col">
+                              {/* height width setup for the mobile */}
+                              <div
+                                    style={{
+                                          height: isTab ? 200 : isMobile ? 200 : 250,
+                                          width: isTab ? 200 : isMobile ? 200 : 250,
                                     }}
-                                    style={{ height: "100%", width: "100%" }}
-                              />
-                              {/* Display the current message */}
-                              <h1 className="text-white text-center font-bold mt-1 text-xl">
-                                    {message.text}
-                              </h1>
+                              >
+                                    {/* Lottie animation component */}
+                                    <Lottie
+                                          loop={animationDefaultOptions.loop}
+                                          autoplay={animationDefaultOptions.autoplay}
+                                          animationData={animationDefaultOptions.animationData}
+                                          rendererSettings={{
+                                                preserveAspectRatio:
+                                                      animationDefaultOptions.rendererSettings.preserveAspectRatio,
+                                          }}
+                                          style={{ height: "100%", width: "100%" }}
+                                    />
+                                    {/* Display the current message */}
+                                    <h1 className="text-white text-center font-bold mt-1 text-xl">
+                                          {message.text}
+                                    </h1>
+
+                              </div>
+
 
                         </div>
 
 
+
+
                   </div>
+            );
+      };
 
-
-
-
-            </div>
-      );
-};
-
-export default WaitingToJoiningScreen;
+      export default WaitingToJoiningScreen;
