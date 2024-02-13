329e1f950b8679f29ab6280f3f17a2bcf0afc6f6
Habibur1112223 <webdevelopment.habib2023@gmail.com>
2024-02-13 16:41:11 +0600

    Cleanup function to clear the interval on component unmount

--- src/Meeting/components/screens/WaitingToJoiningScreen.jsx
+++ src/Meeting/components/screens/WaitingToJoiningScreen.jsx
@@ -20,53 +20,58 @@ const WaitingToJoiningScreen = () => {
                               : waitingMessages[s.index + 1]
                   );
             }, 3000);
+            // Cleanup function to clear the interval on component unmount
+            return () => {
+                  clearInterval(intervalRef.current);
+            };
+      }, []);
 
-            // JSX to render the WaitingToJoinScreen component
-            return (
-                  <div
-                        className="bg-gray-800"
-                        style={{
-                              display: "flex",
-                              alignItems: "center",
-                              justifyContent: "center",
-                              height: "100vh",
-                              // backgroundColor: theme.palette.darkTheme.main,
-                        }}
-                  >
-                        <div className="flex flex-col">
-                              {/* height width setup for the mobile */}
-                              <div
-                                    style={{
-                                          height: isTab ? 200 : isMobile ? 200 : 250,
-                                          width: isTab ? 200 : isMobile ? 200 : 250,
+      // JSX to render the WaitingToJoinScreen component
+      return (
+            <div
+                  className="bg-gray-800"
+                  style={{
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        height: "100vh",
+                        // backgroundColor: theme.palette.darkTheme.main,
+                  }}
+            >
+                  <div className="flex flex-col">
+                        {/* height width setup for the mobile */}
+                        <div
+                              style={{
+                                    height: isTab ? 200 : isMobile ? 200 : 250,
+                                    width: isTab ? 200 : isMobile ? 200 : 250,
+                              }}
+                        >
+                              {/* Lottie animation component */}
+                              <Lottie
+                                    loop={animationDefaultOptions.loop}
+                                    autoplay={animationDefaultOptions.autoplay}
+                                    animationData={animationDefaultOptions.animationData}
+                                    rendererSettings={{
+                                          preserveAspectRatio:
+                                                animationDefaultOptions.rendererSettings.preserveAspectRatio,
                                     }}
-                              >
-                                    {/* Lottie animation component */}
-                                    <Lottie
-                                          loop={animationDefaultOptions.loop}
-                                          autoplay={animationDefaultOptions.autoplay}
-                                          animationData={animationDefaultOptions.animationData}
-                                          rendererSettings={{
-                                                preserveAspectRatio:
-                                                      animationDefaultOptions.rendererSettings.preserveAspectRatio,
-                                          }}
-                                          style={{ height: "100%", width: "100%" }}
-                                    />
-                                    {/* Display the current message */}
-                                    <h1 className="text-white text-center font-bold mt-1 text-xl">
-                                          {message.text}
-                                    </h1>
-
-                              </div>
-
+                                    style={{ height: "100%", width: "100%" }}
+                              />
+                              {/* Display the current message */}
+                              <h1 className="text-white text-center font-bold mt-1 text-xl">
+                                    {message.text}
+                              </h1>
 
                         </div>
 
 
-
-
                   </div>
-            );
-      };
 
-      export default WaitingToJoiningScreen;
+
+
+
+            </div>
+      );
+};
+
+export default WaitingToJoiningScreen;
