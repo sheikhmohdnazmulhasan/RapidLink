ab2225ec01be824c15859d496d904f8c82ec2821
Habibur1112223 <webdevelopment.habib2023@gmail.com>
2024-02-13 16:48:21 +0600

    Display a message indicating that the user left the meeting

--- src/Meeting/components/screens/LeaveScreen.jsx
+++ src/Meeting/components/screens/LeaveScreen.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 const LeaveScreen = () => {
       return (
-              // Styling for the background and layout
-              <div className="bg-gray-800 h-screen flex flex-col flex-1 items-center justify-center">
+            // Styling for the background and layout
+            <div className="bg-gray-800 h-screen flex flex-col flex-1 items-center justify-center">
+                  {/* Display a message indicating that the user left the meeting */}
+                  <h1 className="text-white text-4xl">You left the meeting!</h1>
 
-                  </div>
+            </div>
       );
 };
 
