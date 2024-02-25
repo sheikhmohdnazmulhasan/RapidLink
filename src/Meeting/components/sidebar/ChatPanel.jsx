6aa1d6d2c4d300a5f6b0c2f48d702cca8c080253
Habibur1112223 <webdevelopment.habib2023@gmail.com>
2024-02-25 16:38:36 +0600

    Access the meeting context using useMeeting hook

--- src/Meeting/components/sidebar/ChatPanel.jsx
+++ src/Meeting/components/sidebar/ChatPanel.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import { HiMiniPaperAirplane } from "react-icons/hi2";
 
-const ChatPanel = () => {
+import { useMeeting, usePubSub } from "@videosdk.live/react-sdk";
+import React, { useEffect, useRef, useState } from "react";
+import { formatAMPM, json_verify, nameTructed } from "../../utils/helper";
+
+// set up the chatMessage object call
+const ChatMessage = ({ senderId, senderName, text, timestamp }) => {
+      // Access the meeting context using useMeeting hook
+      const mMeeting = useMeeting();
+      // Retrieve the local participant's ID
+      const localParticipantId = mMeeting?.localParticipant?.id;
+      // Check if the message is from the local participant
+      const localSender = localParticipantId === senderId;
       return (
-            <div>
-                  
-            </div>
-      );
-};
-
-export default ChatPanel;
+            <div
+                  // Outer container with dynamic styling for message alignment
+                 
