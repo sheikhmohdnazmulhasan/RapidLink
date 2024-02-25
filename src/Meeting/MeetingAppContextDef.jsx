e00f3a14634daf5a67f9dcee9fc4b61b94448196
Oleei-Ullah <oleeiullah.coder.97@gmail.com>
2024-02-25 18:10:32 +0600

    meetingapp provider exported

--- src/Meeting/MeetingAppContextDef.jsx
+++ src/Meeting/MeetingAppContextDef.jsx
@@ -4,6 +4,45 @@ import { useContext, createContext, useState, useEffect, useRef } from "react";
 
 export const MeetingAppContext = createContext();
 
+
+export const MeetingAppProvider = ({ children }) => {
+    const [raisedHandsParticipants, setRaisedHandsParticipants] = useState([]);
+    const [sideBarMode, setSideBarMode] = useState(null);
+    const [pipMode, setPipMode] = useState(false);
+
+    const useRaisedHandParticipants = () => {
+      const raisedHandsParticipantsRef = useRef();
+
+      const participantRaisedHand = (participantId) => {
+        const raisedHandsParticipants = [...raisedHandsParticipantsRef.current];
+
+        const newItem = { participantId, raisedHandOn: new Date().getTime() };
+
+        const participantFound = raisedHandsParticipants.findIndex(
+          ({ participantId: pID }) => pID === participantId
+        );
+
+        if (participantFound === -1) {
+          raisedHandsParticipants.push(newItem);
+        } else {
+          raisedHandsParticipants[participantFound] = newItem;
+        }
+
+        setRaisedHandsParticipants(raisedHandsParticipants);
+      };
+
+      useEffect(() => {
+        raisedHandsParticipantsRef.current = raisedHandsParticipants;
+      }, [raisedHandsParticipants]);
+
+      const _handleRemoveOld = () => {
+        const raisedHandsParticipants = [...raisedHandsParticipantsRef.current];
+
+        const now = new Date().getTime();
+
+        const persisted = raisedHandsParticipants.filter(({ raisedHandOn }) => {
+          return parseInt(raisedHandOn) + 15000 > parseInt(now);
+        });
 export default function MeetingAppContextDef() {
   return (
     <div>MeetingAppContextDef</div>
