398f74b046a2243f218c04505d3bcf58ebc69746
Oleei-Ullah <oleeiullah.coder.97@gmail.com>
2024-02-25 18:09:35 +0600

    meeting context initialized

---
+++ src/Meeting/MeetingAppContextDef.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { useContext, createContext, useState, useEffect, useRef } from "react";
+
+export const MeetingAppContext = createContext();
+
+export default function MeetingAppContextDef() {
+  return (
+    <div>MeetingAppContextDef</div>
+  )
+}
