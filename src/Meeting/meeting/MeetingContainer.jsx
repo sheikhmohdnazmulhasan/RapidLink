81252e8b64af88099b5881e9df3a6e7edc9b8293
Oleei-Ullah <oleeiullah.coder.97@gmail.com>
2024-02-25 18:00:19 +0600

    state defined for meetings

--- src/Meeting/meeting/MeetingContainer.jsx
+++ src/Meeting/meeting/MeetingContainer.jsx
@@ -1,6 +1,34 @@
 import React from 'react'
 
 export default function MeetingContainer() {
+
+    const { useRaisedHandParticipants } = useMeetingAppContext();
+    const { getVideoTrack } = useMediaStream();
+
+    const bottomBarHeight = 60;
+
+    const [containerHeight, setContainerHeight] = useState(0);
+    const [containerWidth, setContainerWidth] = useState(0);
+    const [localParticipantAllowedJoin, setLocalParticipantAllowedJoin] =
+      useState(null);
+    const [meetingErrorVisible, setMeetingErrorVisible] = useState(false);
+    const [meetingError, setMeetingError] = useState(false);
+
+    const mMeetingRef = useRef();
+    const containerRef = createRef();
+    const containerHeightRef = useRef();
+    const containerWidthRef = useRef();
+
+    useEffect(() => {
+      containerHeightRef.current = containerHeight;
+      containerWidthRef.current = containerWidth;
+    }, [containerHeight, containerWidth]);
+
+    const isMobile = useIsMobile();
+    const isTab = useIsTab();
+    const isLGDesktop = useMediaQuery({ minWidth: 1024, maxWidth: 1439 });
+    const isXLDesktop = useMediaQuery({ minWidth: 1440 });
+
   return (
     <div>MeetingContainer</div>
   )
