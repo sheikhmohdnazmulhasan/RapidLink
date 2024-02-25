5ea8f49466a29bca4a2c03aa54d75d5e6669e695
ullah <oleeiullah.coder.97@gmail.com>
2024-02-25 17:44:27 +0600

    perrow grid counted

--- src/Meeting/components/ParticipantGrid.jsx
+++ src/Meeting/components/ParticipantGrid.jsx
@@ -5,6 +5,30 @@ const MemoizedParticipant = React.memo(
     }
   );
 
+  const { sideBarMode } = useMeetingAppContext();
+  const isMobile = window.matchMedia(
+    "only screen and (max-width: 768px)"
+  ).matches;
+
+  const perRow =
+    isMobile || isPresenting
+      ? participantIds.length < 4
+        ? 1
+        : participantIds.length < 9
+        ? 2
+        : 3
+      : participantIds.length < 5
+      ? 2
+      : participantIds.length < 7
+      ? 3
+      : participantIds.length < 9
+      ? 4
+      : participantIds.length < 10
+      ? 3
+      : participantIds.length < 11
+      ? 4
+      : 4;
+
 export default function ParticipantGrid() {
   return (
     <div>ParticipantGrid</div>
